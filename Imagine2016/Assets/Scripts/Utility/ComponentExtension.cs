5293e3ff5a07df5e83499e611710154cee615eb6
tom10987 <tom10987@gmail.com>
2016-02-18 09:49:07 +0900

    project upload

---
+++ Imagine2016/Assets/Scripts/Utility/ComponentExtension.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+static class ComponentExtension {
+
+  /// <summary> <see cref="Component.GetComponentsInChildren{T}"/>
+  /// で取得できる配列から親オブジェクト以外を取り出す </summary>
+  public static IEnumerable<T> GetComponentsAllChildren<T>(this Component parent) where T : Component {
+    var components = parent.GetComponentsInChildren<T>();
+    return components.Where(component => component.transform != parent.transform);
+  }
+
+  /// <summary> <see cref="Component.GetComponentsInChildren{T}"/>
+  /// で取得できる配列から、直下にある子オブジェクトのみを取り出す </summary>
+  public static IEnumerable<T> GetComponentsOnlyChildren<T>(this Component parent) where T : Component {
+    var children = parent.GetComponentsInChildren<T>();
+    return children.Where(child => child.transform.parent == parent.transform);
+  }
+}
---
+++ Library/Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using Game.Models;
+
+class Debugger {
+  static void Main(string[] args) {
+    Console.WriteLine("----- debugger start\n");
+    var h = new hoge();
+    h.f();
+    Console.WriteLine("\n----- debugger finish");
+  }
+}
