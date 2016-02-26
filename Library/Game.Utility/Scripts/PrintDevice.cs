771d94f8788babeb3d500c58fd90d699daad0044
tom10987 <tom10987@gmail.com>
2016-02-26 16:45:35 +0900

    add: Library component print image

---
+++ Library/Game.Utility/Scripts/PrintDevice.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Drawing;
+using System.Drawing.Printing;
+
+namespace Game.Utility {
+
+  public static class PrintDevice {
+    static Image _image = null;
+
+    static bool BindImage(string path) {
+      _image = Image.FromFile(path);
+      return _image != null;
+    }
+
+    static void PrintImage() {
+      var pd = new PrintDocument();
+      pd.DocumentName = "";
+      pd.PrintPage += new PrintPageEventHandler(PrintEventAction);
+      pd.Print();
+    }
+
+    public static bool PrintRequest(string path) {
+      if (!BindImage(path)) { return false; }
+      PrintImage();
+      _image = null;
+      return true;
+    }
+
+    static void PrintEventAction(object sender, PrintPageEventArgs args) {
+      var image = _image;
+      args.Graphics.DrawImage(image, args.MarginBounds);
+      args.HasMorePages = false;
+      image.Dispose();
+    }
+  }
+}
--- Library/Game.Utility/Scripts/hoge.cs
+++
@@ -1,3 +0,0 @@
-﻿
-namespace Game.Utility {
-}
