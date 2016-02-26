df2bc09424bba796c6a7f7f95262efc6f0908fae
tom10987 <tom10987@gmail.com>
2016-02-26 15:02:38 +0900

    add: NyARToolKit Library Import

---
+++ Unity.Imagine/Assets/Scripts/AR/ARCamera.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+using System.Collections.Generic;
+using NyARUnityUtils;
+using jp.nyatla.nyartoolkit.cs.markersystem;
+
+public class ARCamera : SingletonBehaviour<ARCamera> {
+
+  NyARUnityMarkerSystem _arSystem = null;
+  public NyARUnityMarkerSystem arSystem { get { return _arSystem; } }
+
+  NyARUnityWebCam _arCamera = null;
+  public NyARUnityWebCam arCamera { get { return _arCamera; } }
+
+  List<ARModel> _models = null;
+  public List<ARModel> models { get { return _models; } }
+
+  [SerializeField]
+  GameObject _panel = null;
+
+  protected override void Awake() {
+    base.Awake();
+    if (WebCamTexture.devices.Length <= 0) { return; }
+
+    var wcTexture = new WebCamTexture(320, 240, 15);
+    _arCamera = NyARUnityWebCam.createInstance(wcTexture);
+
+    var config = new NyARMarkerSystemConfig(_arCamera.width, _arCamera.height);
+    _arSystem = new NyARUnityMarkerSystem(config);
+
+    _panel.GetComponent<Renderer>().material.mainTexture = wcTexture;
+    _arSystem.setARBackgroundTransform(_panel.transform);
+    _arSystem.setARCameraProjection(GetComponent<Camera>());
+
+    _models = new List<ARModel>();
+  }
+
+  void Start() {
+    _arCamera.start();
+  }
+
+  void Update() {
+    _arCamera.update();
+    _arSystem.update(_arCamera);
+  }
+}
---
+++ Unity.Imagine/Assets/Scripts/AR/ARModel.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class ARModel : MonoBehaviour {
+
+  public int id { get; private set; }
+
+  [SerializeField]
+  Texture2D _marker = null;
+
+  [SerializeField]
+  [Range(10f, 100f)]
+  float _scale = 50f;
+
+  void Start() { if (_marker != null) { StartCoroutine(UpdateModel()); } }
+
+  IEnumerator UpdateModel() {
+    var ar = ARCamera.instance;
+    id = ar.arSystem.addARMarker(_marker, 16, 25, 80);
+    ar.models.Add(this);
+    transform.localScale = Vector3.one * _scale;
+
+    while (true) {
+      if (ar.arSystem.isExistMarker(id)) {
+        transform.position = Vector3.zero;
+        ar.arSystem.setMarkerTransform(id, transform);
+        transform.Rotate(Vector3.right * 90f);
+      }
+      yield return null;
+    }
+  }
+}
