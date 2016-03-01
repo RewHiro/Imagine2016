2bcdc10e4b5952ad6e1e8f1c793973179f13a622
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-03-02 02:54:07 +0900

    characteパラメータ設定のスクリプトを用意

---
+++ Unity.Imagine/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    CharacterParameter _characterParamter;
+
+    public CharacterParameter getCharacterParamter
+    {
+        get
+        {
+            return _characterParamter;
+        }
+    }
+
+    void Start()
+    {
+        //TODO：外部からパラメータを読み込む
+    }
+}
---
+++ Unity.Imagine/Assets/Scripts/Character/CharacterParameter.cs
@@ -0,0 +1,8 @@
+﻿public struct CharacterParameter
+{
+    public uint type { get; set; }
+    public uint costumeID { get; set; }
+    public uint attack { get; set; }
+    public uint defense { get; set; }
+    public uint speed { get; set; }
+}
