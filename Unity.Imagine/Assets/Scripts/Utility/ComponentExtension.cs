6934d604f389679d6b5b5f90107bf0d2914194e2
tom10987 <tom10987@gmail.com>
2016-03-02 13:11:58 +0900

    update: シーン名の変更、コメント追記

--- Unity.Imagine/Assets/Scripts/Utility/ComponentExtension.cs
+++ Unity.Imagine/Assets/Scripts/Utility/ComponentExtension.cs
@@ -3,13 +3,20 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Linq;
 
+//------------------------------------------------------------
+// TIPS:
+// Component クラスの外部拡張
+// Component からの派生クラス（MonoBehaviour を継承したクラス含む）しか取り出せない
+//
+//------------------------------------------------------------
+
 static class ComponentExtension {
 
   /// <summary> <see cref="Component.GetComponentsInChildren{T}"/>
   /// で取得できる配列から親オブジェクト以外を取り出す </summary>
   public static IEnumerable<T> GetComponentsAllChildren<T>(this Component parent) where T : Component {
-    var components = parent.GetComponentsInChildren<T>();
-    return components.Where(component => component.transform != parent.transform);
+    var children = parent.GetComponentsInChildren<T>();
+    return children.Where(child => child.transform != parent.transform);
   }
 
   /// <summary> <see cref="Component.GetComponentsInChildren{T}"/>
