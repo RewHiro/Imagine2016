2913ab86b01e91d2d532f4e7ab7aeddc93c4822f
tom10987 <tom10987@gmail.com>
2016-03-05 12:48:22 +0900

    update: AR 機能の一部を分割、攻勢を変更

---
+++ Unity.Imagine/Assets/Scripts/AR/ARModelManager.cs
@@ -0,0 +1,8 @@
+﻿
+using System.Collections.Generic;
+
+public class ARModelManager : SingletonBehaviour<ARModelManager> {
+
+  readonly List<ARModel> _models = new List<ARModel>();
+  public List<ARModel> models { get { return _models; } }
+}
