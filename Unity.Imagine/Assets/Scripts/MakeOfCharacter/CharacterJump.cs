45417fac7b2e8772a8c5575ca5ec5d924ecdf241
ooHIROoo <hiro1215.turquoise@gmail.com>
2016-03-16 00:03:35 +0900

    クリエイト画面でキャラをタップするとジャンプする

--- Unity.Imagine/Assets/Scripts/MakeOfCharacter/CharacterJump.cs
+++ Unity.Imagine/Assets/Scripts/MakeOfCharacter/CharacterJump.cs
@@ -7,6 +7,10 @@ public class CharacterJump : MonoBehaviour
     [SerializeField]
     private float _jumpForce = 100.0f;
 
+    int LAYER_MASK = 0;
+
+    bool _isJump = false;
+
     //private float _randomTime = 3.0f;
 
     //[SerializeField]
@@ -16,30 +20,43 @@ public class CharacterJump : MonoBehaviour
 
     void Start()
     {
-
+        string[] hitLayerNames = new string[1];
+        hitLayerNames[0] = "TransparentFX";
+        LAYER_MASK = LayerMask.GetMask(hitLayerNames);
     }
 
     void Update()
     {
-        //_randomTime -= Time.deltaTime;
-        //if(_randomTime <= 0.0f)
-        //{
-        //    _randomTime = UnityEngine.Random.Range(_minTime, _maxTime);
-        //    JumpAction();
-        //}
+        Gravity();
+        JumpAction();
+    }
+
+    void Gravity()
+    {
+        if (!_isJump) return;
 
         var localPosition = transform.localPosition;
 
-        if (localPosition.y > 0.0f) return;
+        if (localPosition.y > -0.01f) return;
+        Debug.Log("HIT");
         GetComponent<Rigidbody>().useGravity = false;
         GetComponent<Rigidbody>().velocity = Vector3.zero;
         transform.localPosition = new Vector3(localPosition.x, 0, localPosition.z);
 
+        _isJump = false;
     }
 
-    public void JumpAction()
+    void JumpAction()
     {
+        if (_isJump) return;
+
+        if (!TouchController.IsTouchBegan()) return;
+
+        RaycastHit raycastHit;
+        if (!TouchController.IsRaycastHitWithLayer(out raycastHit, LAYER_MASK)) return;
+
         GetComponent<Rigidbody>().AddForce(Vector3.up * _jumpForce);
         GetComponent<Rigidbody>().useGravity = true;
+        _isJump = true;
     }
 }
