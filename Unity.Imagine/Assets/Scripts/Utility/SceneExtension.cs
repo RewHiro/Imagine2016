53a161b91ad3191575a4195a0a1671dc454bf990
tom10987 <tom10987@gmail.com>
2016-03-17 11:56:08 +0900

    update: シーン切り替え実装

--- Unity.Imagine/Assets/Scripts/Utility/SceneExtension.cs
+++ Unity.Imagine/Assets/Scripts/Utility/SceneExtension.cs
@@ -1,19 +1,27 @@
 ﻿
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public enum GameScene {
   Title,
   Menu,
   Create,
   Printer,
-  MiniGame,
+  MiniGames,
 
   Max, None = -1,
 }
 
 public static class SceneExtension {
 
-  public static bool ChangeScene(this GameScene scene) {
-    return false;
+  /// <summary> シーンを切り替える </summary>
+  public static void ChangeScene(this GameScene scene) {
+    SceneManager.LoadScene(scene.ToString());
+  }
+
+  /// <summary> 実行中のシーンを全て取得 </summary>
+  public static IEnumerable<Scene> GetAllActiveScenes() {
+    var count = SceneManager.sceneCount;
+    for (var i = 0; i < count; ++i) { yield return SceneManager.GetSceneAt(i); }
   }
 }
