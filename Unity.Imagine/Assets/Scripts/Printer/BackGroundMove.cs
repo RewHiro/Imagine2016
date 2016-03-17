7a60a5637f8d745d564a69439b2f5e2cace2a139
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-03-17 12:26:43 +0900

    印刷カラー設定いじりました。 背景移動するスクリプト追加

---
+++ Unity.Imagine/Assets/Scripts/Printer/BackGroundMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundMove : MonoBehaviour {
+
+    [SerializeField, Tooltip("X方向に進むスピード, もう一方と同じスピードにすること")]
+    private float _moveSpeed = 0.1f;
+    [SerializeField, Tooltip("スタートの背景のX座標")]
+    private int _startPosX;
+
+    /// <summary>
+    /// キャンバスのサイズ
+    /// </summary>
+    private const int _returnPosX = 1920;
+     
+	void Start ()
+    {
+        transform.localPosition = new Vector3(_startPosX, 0.0f, 0.0f);
+	}
+	
+	void Update ()
+    {
+        transform.Translate(_moveSpeed, 0.0f, 0.0f);
+        if(transform.localPosition.x > _returnPosX)
+        {
+            transform.localPosition = new Vector3(-_returnPosX, 0.0f, 0.0f);
+        }
+	}
+}
