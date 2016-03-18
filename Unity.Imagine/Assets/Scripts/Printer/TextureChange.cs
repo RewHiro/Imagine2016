5766d7118863dab4425205c8b538878b7f5b1855
Satoaki131 <akinori_0131@yahoo.co.jp>
2016-03-18 17:06:58 +0900

    背景画像追加、背景のPrefab追加

---
+++ Unity.Imagine/Assets/Scripts/Printer/TextureChange.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextureChange : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] _panel;
+
+    [SerializeField]
+    private Sprite _backGroundTexture;
+
+	void Start ()
+    {
+        foreach(var panel in _panel)
+        {
+            panel.GetComponent<Image>().sprite = _backGroundTexture;
+        }
+    }
+	
+	
+}
