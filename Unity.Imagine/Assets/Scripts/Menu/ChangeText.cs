9e685e576fa543dd46de416d72a4b25b19e5f215
zun-ferrum <dry.tm.fe@icloud.com>
2016-03-19 23:55:16 +0900

    Menu処理変更

---
+++ Unity.Imagine/Assets/Scripts/Menu/ChangeText.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+
+public class ChangeText : MonoBehaviour
+{
+
+    private List<String> _text = new List<string>();
+   
+    void Start()
+    {
+        _text.Add("このがめんでは\nゲームをえらべるよ\n3つのゲームからえらんでね");
+        _text.Add("このゲームはおたがいのぼうしを\nねらってたまをうちあうゲームだよ。\nはやいCuboがとくいだよ\nよかったらOKをおしてね");
+        _text.Add("このゲームは\nまだあそべないよ");
+        _text.Add("このゲームは\nまだあそべないよ");
+        _text.Add("ゲームをじどうでえらんでるよ");
+        _text.Add("これはゲームをえらんだときに\nどんなCuboがつよいのかみれるよ。");
+
+        ChangeExplanationText(0);
+    }
+
+   
+    void Update()
+    {
+
+    }
+
+
+    public void ChangeExplanationText(int _selectNum)
+    {
+        this.GetComponent<Text>().text = _text[_selectNum];
+    }
+}
