34f0eb304865f5894f047c52a5bafea828b27fc0
tom10987 <tom10987@gmail.com>
2016-03-21 20:02:27 +0900

    fix: シーンが変わると再生できなくなる不具合修正

--- Unity.Imagine/Assets/Scripts/Utility/ComponentExtension.cs
+++ Unity.Imagine/Assets/Scripts/Utility/ComponentExtension.cs
@@ -12,16 +12,16 @@ using System.Linq;
 
 static class ComponentExtension {
 
-  /// <summary> <see cref="Component.GetComponentsInChildren{T}"/>
+  /// <summary> <see cref="Component.GetComponentsInChildren{T}()"/>
   /// で取得できる配列から親オブジェクト以外を取り出す </summary>
-  public static IEnumerable<T> GetComponentsAllChildren<T>(this Component parent) where T : Component {
+  public static IEnumerable<T> GetAllChildren<T>(this Component parent) where T : Component {
     var children = parent.GetComponentsInChildren<T>();
     return children.Where(child => child.transform != parent.transform);
   }
 
-  /// <summary> <see cref="Component.GetComponentsInChildren{T}"/>
+  /// <summary> <see cref="Component.GetComponentsInChildren{T}()"/>
   /// で取得できる配列から、直下にある子オブジェクトのみを取り出す </summary>
-  public static IEnumerable<T> GetComponentsOnlyChildren<T>(this Component parent) where T : Component {
+  public static IEnumerable<T> GetOnlyChildren<T>(this Component parent) where T : Component {
     var children = parent.GetComponentsInChildren<T>();
     return children.Where(child => child.transform.parent == parent.transform);
   }
