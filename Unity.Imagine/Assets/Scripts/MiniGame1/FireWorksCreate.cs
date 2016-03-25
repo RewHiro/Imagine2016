a558911a98d8ec08c2ced3955be86f2e656f4663
kazukin134 <kazukin1345@gmail.com>
2016-03-25 13:50:33 +0900

    pull更新

---
+++ Unity.Imagine/Assets/Scripts/MiniGame1/FireWorksCreate.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireWorksCreate : MonoBehaviour {
+
+    [SerializeField]
+    float _coolDownTime = 1;
+
+    float _time;
+
+    [SerializeField]
+    GameObject _particle;
+
+	void Start ()
+    {
+        _time = _coolDownTime;
+	}
+	
+	void Update ()
+    {
+        if (CresteCoolDown())
+        {
+            Create();
+            _time = _coolDownTime;
+        }
+	}
+
+    bool CresteCoolDown()
+    {
+        _time -= Time.deltaTime;
+
+        
+        if (_time <= 0) return true;
+        return false;
+
+    }
+
+    void Create()
+    {
+        GameObject particleObj = Instantiate(_particle);
+        particleObj.transform.SetParent(gameObject.transform, false);
+    }
+
+}
